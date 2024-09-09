98c9c212a307c829301c3037a800c9ad74d735cd
Satyam2472 <gupta.81@iitj.ac.in>
2024-09-09 17:38:57 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,10 @@ const port = process.env.PORT || 3000
 
 // Explicitly allowing your frontend Netlify URL
 const corsOptions = {
-  origin: "*",
-  // origin: "https://fk-product-detail-scraper.netlify.app",
-  methods: "GET,POST,OPTIONS",
-  allowedHeaders: ["Content-Type"],
+  // origin: "*",
+  origin: 'https://fk-scraper-1.onrender.com/',
+  methods: 'GET,POST,OPTIONS',
+  allowedHeaders: ['Content-Type'],
   optionsSuccessStatus: 200, // For older browsers
 };
 
