d2b7be752f233e431ca24ab49a99dd85cefcc817
Satyam Kumar Gupta <92547254+Satyam2472@users.noreply.github.com>
2024-09-11 14:51:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,29 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000
 
-// Explicitly allow your frontend URL
-const corsOptions = {
-  origin: "https://fk-scraper-1.onrender.com",  // Frontend URL
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"],
-};
+// // Explicitly allow your frontend URL
+// const corsOptions = {
+//   origin: "https://fk-scraper-1.onrender.com",  // Frontend URL
+//   methods: ["GET", "POST", "OPTIONS"],
+//   allowedHeaders: ["Content-Type"],
+// };
 
-app.use(cors(corsOptions));
-// Handle preflight (OPTIONS) requests
-app.options('*', cors(corsOptions)); // Preflight handling
+// app.use(cors(corsOptions));
+// // Handle preflight (OPTIONS) requests
+// app.options('*', cors(corsOptions)); // Preflight handling
+
+app.use((req, res, next) => {
+  res.origin('Access-Control-Allow-Origin', 'https://fk-scraper-first.vercel.app');
+  res.method('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 
 // Use security and caching headers
